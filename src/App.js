0687699805abfe8826e44613117c8ff4adc0dfcd
Preetam7064 <preetamsahu.cse.2019@nist.edu>
2023-07-17 14:08:45 +0530

    Library ui and font

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './styles/app.scss';
 //adding components
 import Player from './components/Player';
 import Song from './components/Song';
+import Library from './components/Library';
 //importing data
 import data from './data';
 
@@ -19,6 +20,7 @@ function App() {
 				setIsPlaying={setIsPlaying}
 				currentSong={currentSong}
 			/>
+			<Library songs={songs} />
 		</div>
 	);
 }
