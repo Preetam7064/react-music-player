b8a3ad625c93d321ebfbfb7e1100c465fee51254
Preetam7064 <preetamsahu.cse.2019@nist.edu>
2023-07-17 09:25:50 +0530

    added our state and styled the container

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 //adding styles
 import './styles/app.scss';
 //adding components
 import Player from './components/Player';
 import Song from './components/Song';
+//importing data
+import data from './data';
 
 function App() {
+	const [songs, setSongs] = useState(data());
+	const [currentSong, setCurrentSong] = useState(songs[0]);
 	return (
 		<div className="App">
-			<Song />
+			<Song currentSong={currentSong} />
 			<Player />
 		</div>
 	);
